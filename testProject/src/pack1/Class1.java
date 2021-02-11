5bc883690a8b25e0d6b1297d9d4b4d226e9a0e48
kpg <kanupriya_garg@hotmail.com>
2021-02-11 10:41:30 -0500

    added class1

--- testProject/src/pack1/Class1.java
+++ testProject/src/pack1/Class1.java
@@ -1,5 +1,38 @@
 package pack1;
 
 public class Class1 {
+	
+	private int fnum;
+	private int snum;
+		
+	public void setfnum(int fnum ) 
+	{
+		this.fnum=fnum;
+	}
+	
+	public void setsnum(int snum)
+	{
+	this.snum=snum;	
+		
+	}
+	
+	public int getfnum() {
+		
+		return fnum;
+		
+	}
+	
+	public int snum() {
+		return snum;
+		
+	}
+	
+	public int getSum(int a,int b) {
+		
+		int sum= a+b;
+		return sum;
+		
+	}
+	
 
 }
